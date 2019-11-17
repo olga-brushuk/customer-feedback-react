74cd071d9c8c7a3992ac0f3fd84f4b936e54a0e3
Olga <32768271+olha-brushuk@users.noreply.github.com>
2019-11-17 23:53:54 +0000

    Form input fields addition
    Addedform fields and handlers

--- src/App.js
+++ src/App.js
@@ -2,24 +2,83 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class CustomerFeedback extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { // set default states
+      formControls: {
+          email: {
+            value: ''
+          },
+          name: {
+            value: ''
+          },
+          comment: {
+            value: ''
+          }
+      }
+    };
+
+    //this.handleNameChange = this.handleNameChange.bind(this);  
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  changeHandler = event => {
+      
+      const name = event.target.name;
+      const value = event.target.value;
+    
+      this.setState({
+        formControls: {
+            ...this.state.formControls,
+            [name]: {
+            ...this.state.formControls[name],
+            value
+          }
+        }
+      });
+  }
+
+  handleSubmit(event) {
+    alert('Email: ' + this.state.formControls.email.value);
+    alert('Name: ' + this.state.formControls.name.value);
+    alert('Comment: ' + this.state.formControls.comment.value);
+    //alert('Comment: ' + this.state.comment);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="email" 
+                     name="email" 
+                     value={this.state.formControls.email.value} 
+                     onChange={this.changeHandler} 
+              />
+
+              <input type="text" 
+                     name="name" 
+                     value={this.state.formControls.name.value} 
+                     onChange={this.changeHandler} 
+              />
+
+              <textarea 
+                     name="comment" 
+                     value={this.state.formControls.comment.value} 
+                     onChange={this.changeHandler} 
+              />
+
+        {/* <textarea name="comment" onChange={this.handleCommentChange} value={this.state.comment} placeholder="Please enter your comment" /> */} 
+
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CustomerFeedback />
   );
 }
 
