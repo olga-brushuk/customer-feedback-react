8a0514db5e31ba413e7c8ab5c92b8b00e78d56f2
Olga Brushuk <32768271+olga-brushuk@users.noreply.github.com>
2019-11-24 21:22:04 +0000

    Updates

---
+++ precache-manifest.9ecc7a557261fb36a17ff39dfe54b5d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd4b1f7d14b8287a3b5466adeae2b46",
+    "url": "/customer-feedback-react/index.html"
+  },
+  {
+    "revision": "bd76469c6758c0e46ac9",
+    "url": "/customer-feedback-react/static/css/main.7471f1b7.chunk.css"
+  },
+  {
+    "revision": "4e09fac45387dff2c785",
+    "url": "/customer-feedback-react/static/js/2.b33b5689.chunk.js"
+  },
+  {
+    "revision": "bd76469c6758c0e46ac9",
+    "url": "/customer-feedback-react/static/js/main.4975a831.chunk.js"
+  },
+  {
+    "revision": "cd0a0c47b7d48d04a743",
+    "url": "/customer-feedback-react/static/js/runtime-main.3ab71b9f.js"
+  }
+]);
